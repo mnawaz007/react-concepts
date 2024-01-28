b45589427ceca7ee698899f22685dfff48d1286a
Musa Khan <altanmusakurt@gmail.com>
2024-01-28 23:17:18 +0500

    deleted extra

---
+++ src/header/header.jsx
@@ -0,0 +1,9 @@
+import core from '../assets/react-core-concepts.png'
+export default function Header(){
+    return(
+        <>
+        <h1>React Core Concepts</h1>
+        <img src= {core} alt="main image" />
+        </>
+    )
+}
