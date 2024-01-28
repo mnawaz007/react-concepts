de6886c555ec839251225ee4a3ad9aee726f6c29
Musa Khan <altanmusakurt@gmail.com>
2024-01-28 23:37:40 +0500

    react bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Header from './header/header.jsx';
 export default function App (){
+    return(
+        <>
+        <Header />
+        </>
+    )
     
 }
--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,9 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import core from '../assets/react-core-concepts.png'
 export default function Header(){
     return(
         <>
-        <h1>React Core Concepts</h1>
+        <container>
+            <row> 
+                <col> 
+                <h1>React Core Concepts</h1>
         <img src= {core} alt="main image" />
+                </col>
+             </row>
+            
+        </container>
+        
         </>
     )
 }
